cfd21f34f7a8645ea81555565c37c2f098325853
hperezs <h.perez.saldivar@gmail.com>
2025-09-05 17:10:15 -0600

    More layout tweaks to services

--- lib/components/NavBar.tsx
+++ lib/components/NavBar.tsx
@@ -7,7 +7,12 @@ const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const NavBar = () => {
   const [showNav, setShowNav] = useState(true);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   const lastScrollY = useRef(0);
+
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,18 +30,25 @@ export const NavBar = () => {
 
   return (
     <nav
-      className={`bg-white shadow-lg fixed w-full top-0 z-50 transition-transform duration-300 ${roboto.className} ${showNav ? "translate-y-0" : "-translate-y-full"}`}
+      className={`bg-white shadow-lg fixed w-full top-0 z-50 transition-transform duration-300 ${
+        roboto.className
+      } ${showNav ? "translate-y-0" : "-translate-y-full"}`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center min-h-16">
           <div className="flex items-center">
             <div className="text-2xl font-bold text-primary py-2">
               <Link href="/">
-                <img src={`${process.env.PAGES_BASE_PATH || ""}/internationalsce_logo.png`} alt="SCE Group Logo" />
+                <img
+                  src={`${
+                    process.env.PAGES_BASE_PATH || ""
+                  }/internationalsce_logo.png`}
+                  alt="SCE Group Logo"
+                />
               </Link>
             </div>
           </div>
-          <div className="">
+          <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
               <Link href="/#services">
                 <div className="text-gray-700 hover:text-primary px-3 py-2 transition duration-300">
@@ -55,10 +67,11 @@ export const NavBar = () => {
               </Link>
             </div>
           </div>
-          <div className="hidden">
+          <div className="md:hidden">
             <button
               id="mobile-menu-button"
               className="text-gray-700 hover:text-primary"
+              onClick={toggleMobileMenu}
             >
               <svg
                 className="h-6 w-6"
@@ -75,6 +88,37 @@ export const NavBar = () => {
               </svg>
             </button>
           </div>
+        </div>
+      </div>
+      {/*<!-- Mobile menu --> */}
+      <div
+        id="mobile-menu"
+        className={`md:hidden ${
+          showMobileMenu ? "h-[150px]" : "h-0"
+        } bg-white border-t border-b border-gray-200 shadow-lg/30 transition-all duration-150 ease-out overflow-hidden`}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <a
+            href="/#services"
+            className="block px-3 py-2 text-gray-700 hover:text-primary"
+            onClick={toggleMobileMenu}
+          >
+            Servicios
+          </a>
+          <a
+            href="/about"
+            className="block px-3 py-2 text-gray-700 hover:text-primary"
+            onClick={toggleMobileMenu}
+          >
+            Sobre Nosotros
+          </a>
+          <a
+            href="/#contact"
+            className="block px-3 py-2 text-gray-700 hover:text-primary"
+            onClick={toggleMobileMenu}
+          >
+            Contacto
+          </a>
         </div>
       </div>
     </nav>
--- lib/components/Services.tsx
+++ lib/components/Services.tsx
@@ -12,7 +12,7 @@ export const Services = () => {
       expandedContent:
         "Ofrecemos servicios de jardinería, mantenimiento de aires acondicionados, plomería, pintura y remodelación, con soluciones confiables y de calidad para renovar, mantener y mejorar cualquier espacio.",
       image:
-        "https://images.unsplash.com/photo-1589939705384-5185137a7f0f?w=400&h=300&fit=crop&crop=center",
+        "https://images.unsplash.com/photo-1589939705384-5185137a7f0f?fit=crop&crop=center",
       colors: "from-blue-700/10 to-sky-600/10",
       borderColor: "border-blue-600/20",
       buttonColor: "bg-blue-600 hover:bg-blue-700",
@@ -27,7 +27,7 @@ export const Services = () => {
       expandedContent:
         "Proporcionamos servicios contables integrales que abarcan desde la gestión de impuestos y cumplimiento fiscal hasta la organización financiera y asesoría personalizada, ayudando a tu empresa o proyecto a mantener estabilidad y crecer con confianza.",
       image:
-        "https://images.unsplash.com/photo-1530971013997-e06bb52a2372?w=400&h=300&fit=crop&crop=center",
+        "https://images.unsplash.com/photo-1530971013997-e06bb52a2372?fit=crop&crop=center",
       colors: "from-emerald-200/10 to-cyan-500/10",
       borderColor: "border-teal-600/20",
       buttonColor: "bg-teal-600 hover:bg-teal-700",
@@ -42,8 +42,8 @@ export const Services = () => {
       expandedContent:
         "Nos especializamos en registrar eventos y producir videos profesionales, desde la grabación y edición hasta entrevistas y otros formatos audiovisuales que destacan y dan vida a cada ocasión.",
       image:
-        "https://images.unsplash.com/photo-1575411602736-5e9f24e277d0?w=400&h=300&fit=crop&crop=center",
-      colors: "hover:from-slate-900/70 hover:to-neutral-900",
+        "https://images.unsplash.com/photo-1575411602736-5e9f24e277d0?fit=crop&crop=center",
+      colors: "hover:from-slate-900/80 hover:to-neutral-900",
       borderColor: "border-indigo-600/20",
       buttonColor: "bg-blue-700 hover:bg-blue-600",
       shadowColor: "hover:shadow-slate-700/25",
@@ -52,7 +52,7 @@ export const Services = () => {
     },
   ];
   return (
-    <div className="grid grid-cols-1 gap-8 max-w-3xl mx-auto">
+    <div className="grid grid-cols-1 gap-8 max-w-2xl mx-auto">
       {companies.map((company, index) => (
         <div
           key={index}
@@ -60,49 +60,54 @@ export const Services = () => {
         >
           {/* Main card content - always visible */}
           <div
-            className={`p-5 h-[176px] flex items-center bg-none ${company.bgGradient} ${company.colors} group-hover:items-start space-x- text-left gap-6 hover:h-[495px] md:hover:h-[450px] transition-all duration-500 ease-out`}
+            className={`p-5 h-[176px] bg-none ${company.bgGradient} ${company.colors} overflow-hidden group-hover:items-start text-left hover:h-[500px] md:hover:h-[470px] transition-all duration-500 ease-out`}
           >
-            <img
-              src={company.logo}
-              alt={`${company.name} logo`}
-              className="w-32 h-32 rounded group-hover:align-top"
-            />
-            <div className="flex-1">
-              <div className="h-32 flex justify-center flex-col">
-                <h3
-                  className={`text-xl font-semibold text-slate-900 mb-1 ${company.hoverTextColor}`}
-                >
-                  {company.name}
-                </h3>
-                <p
-                  className={`text-slate-800 text-lg ${company.hoverTextColor}`}
-                >
-                  {company.description}
-                </p>
-              </div>
+            <div className="flex items-center gap-6">
+              <img
+                src={company.logo}
+                alt={`${company.name} logo`}
+                className={
+                  "w-20 h-20 md:w-32 md:h-32 rounded group-hover:align-top"
+                }
+              />
 
-              {/* Expanded content - shown on hover */}
-              <div
-                className={`${roboto.className} hidden group-hover:block opacity-0 transform translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-out`}
-              >
-                <div className="">
-                  <img
-                    src={company.image}
-                    alt={`${company.name} visual`}
-                    className="w-full h-32 object-cover rounded-lg shadow-lg my-4"
-                  />
-                  <p
-                    className={`text-slate-800 text-sm leading-relaxed ${company.hoverTextColor}`}
+              <div className="flex-1">
+                <div className="h-32 flex justify-center flex-col">
+                  <h3
+                    className={`text-xl font-semibold text-slate-900 mb-1 ${company.hoverTextColor}`}
                   >
-                    {company.expandedContent}
+                    {company.name}
+                  </h3>
+                  <p
+                    className={`text-slate-800 text-lg ${company.hoverTextColor}`}
+                  >
+                    {company.description}
                   </p>
-                  <div className="mt-4 flex justify-end">
-                    <button
-                      className={`px-6 py-2 ${company.buttonColor} ml-auto text-white text-sm rounded transition-colors duration-200 cursor-pointer`}
-                    >
-                      Ver Más
-                    </button>
-                  </div>
+                </div>
+              </div>
+            </div>
+            
+            {/* Expanded content - shown on hover */}
+            <div
+              className={`${roboto.className} border-t border-gray-300 mt-4 opacity-0 transform translate-y-4 group-hover:opacity-100 group-hover:translate-y-0 transition-all duration-500 ease-out`}
+            >
+              <div className="">
+                <img
+                  src={company.image}
+                  alt={`${company.name} visual`}
+                  className="w-full h-32 object-cover rounded shadow-lg my-4"
+                />
+                <p
+                  className={`text-slate-800 text-sm leading-relaxed ${company.hoverTextColor}`}
+                >
+                  {company.expandedContent}
+                </p>
+                <div className="mt-4 flex justify-end">
+                  <button
+                    className={`px-6 py-2 ${company.buttonColor} ml-auto text-white text-sm rounded transition-colors duration-200 cursor-pointer`}
+                  >
+                    Ver Más
+                  </button>
                 </div>
               </div>
             </div>
