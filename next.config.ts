d132692bd888932c19087f772a9df91d8b032b7e
hperezs <h.perez.saldivar@gmail.com>
2025-09-05 11:51:57 -0600

    Update next.config to support custom domain (hopefully)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
